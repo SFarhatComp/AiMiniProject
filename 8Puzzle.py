bcd356bfc744c24f008ff051ab0fd5bc85225993
misolaptop <sami_farhat99@hotmail.com>
2023-03-14 10:51:57 -0400

    The 8 Puzzle has been completly finished coded and tested with the input given by the teacher

--- 8Puzzle.py
+++ 8Puzzle.py
@@ -3,17 +3,19 @@ import time
 from typing import List, Tuple
 from copy import deepcopy
 
+# Function Tester is used to make sure that two states are equal
 
 
-def Tester(list_of_state, State_to_be_found) -> bool:  # Function Tester is used to make sure that two states are equal
+def Tester(list_of_state, State_to_be_found) -> bool:
     for state in list_of_state:
         if np.array_equal(State_to_be_found, state):
             return True
     return False
 
+# This generator will generate all the children of any given puzzle
 
-def successor_state_generator(current_state, depth_counter=None, succ_dict=None) -> List[List]: #This generator will generate all the children of any given puzzle
 
+def successor_state_generator(current_state, depth_counter=None, succ_dict=None) -> List[List]:
     successors = []
 
     if depth_counter != None and succ_dict != None:
@@ -24,8 +26,9 @@ def successor_state_generator(current_state, depth_counter=None, succ_dict=None)
             succ_dict[string_current_state] = 0
 
     # Everytime a set of new child is generated, add +1 to a counter. That specific child is at that step
-    possible_moves = ["Up", "Down", "Left", "Right"]
+    # possible_moves = ["Up", "Down", "Left", "Right"]
     # first step is finding where the B is:
+
     x_position_blank = np.argwhere(current_state == 'B')[0][0]
     y_position_blank = np.argwhere(current_state == 'B')[0][1]
 
@@ -110,23 +113,18 @@ def successor_state_generator(current_state, depth_counter=None, succ_dict=None)
                 succ_dict[string_new_stateDown] = depth_counter
 
     return successors, depth_counter
+    # We return the succesor list and the depth counter if needed for the StarA
 
 
-
-def DepthFirstSearch(istate, gstate):  
-
+def DepthFirstSearch(istate, gstate):
     print("DepthFirstSearch has been called")
     open_list = [istate]
     closed_list = []
-
-    print(Tester(open_list, istate))
     # print("THIS IS THE OPEN LIST BEFORE DOING ANYTHING")
     # print(open_list)
     while open_list:
         current_state = open_list.pop()
         closed_list.append(current_state)
-        print("This is the state that is passed to the successor generator\n")
-        print(current_state)
         if np.array_equal(current_state, gstate):
             print(
                 f"Puzzle has been solved, this is the final closed list : ")
@@ -134,8 +132,7 @@ def DepthFirstSearch(istate, gstate):
                 print(state)
                 print("\n \n")
                 print(f"Finished with itteration {i}")
-        return closed_list
-
+            return closed_list
         for successor in successor_state_generator(current_state)[0]:
             if not Tester(open_list, successor) and not Tester(closed_list, successor):
                 open_list.append(successor)
@@ -157,7 +154,7 @@ def BreathFirstSearch(istate, gstate):
                 print(state)
                 print("\n \n")
                 print(f"Finished with itteration {i}")
-        return closed_list
+            return closed_list
 
         for successor in successor_state_generator(current_state)[0]:
             if not Tester(open_list, successor) and not Tester(closed_list, successor):
@@ -169,12 +166,9 @@ def BestFirst(istate, gstate, heuristics):
     print("BestFirst Search Started ")
     open_list = [istate]
     closed_list = []
-    i = 0
     while open_list:
         current_state = open_list.pop(0)
         closed_list.append(current_state)
-        print(f"this is iteration number : {i}")
-        i += 1
         if np.array_equal(current_state, gstate):
             print(
                 f"Puzzle has been solved, this is the final closed list : ")
@@ -182,7 +176,7 @@ def BestFirst(istate, gstate, heuristics):
                 print(state)
                 print("\n \n")
             print(f"Finished with itteration {i}")
-        return closed_list
+            return closed_list
 
         for successor in successor_state_generator(current_state)[0]:
             if not Tester(open_list, successor) and not Tester(closed_list, successor):
@@ -192,25 +186,30 @@ def BestFirst(istate, gstate, heuristics):
 
 
 def Astar(istate, gstate, heuristics):
+
+    # For this search , I am inplementing a depth_counter and a successor_dict.
+    # The goal behind it is everytime the state generator is called, we increment the counter by 1.
+    # In short , the depth is the amount of step needed to get from the Initial state to the current state. Therefore it is the cost.
+    # Then we verify , if that state is not in the dictionnary , we add it in the dictionary as a key with its value being the depth at which it was created
+    # Therefore we get a dictionnary [State x : Depth (cost from starting node to this state)]
+
     print("A* Search Started ")
-    # Start it at -1 because we increment it at each time a current state is poped. However if current state = goal state from first tri then G(n) should be 0
     depth_counter = 0
     successor_dict = {}
     open_list = [istate]
     closed_list = []
-    i = 0
     while open_list:
         current_state = open_list.pop(0)
         closed_list.append(current_state)
 
         if np.array_equal(current_state, gstate):
             print(
-            f"Puzzle has been solved, this is the final closed list : ")
+                f"Puzzle has been solved, this is the final closed list : ")
             for state in closed_list:
                 print(state)
-                print("\n \n")
-                print(f"Finished with itteration {i}")
-        return closed_list
+            print(
+                f"The cost was {successor_dict[''.join(map(str, [column for row in current_state for column in row]))]}")
+            return closed_list
 
         list_of_successor, depth_counter = successor_state_generator(
             current_state, depth_counter, successor_dict)
@@ -223,18 +222,9 @@ def Astar(istate, gstate, heuristics):
             open_list, key=lambda x: heuristics(x, gstate) + successor_dict["".join(
                 map(str, [column for row in x for column in row]))])
 
-        for x in open_list:
-            print(f"The current value of each state is {x} : ")
-
-            print(successor_dict["".join(
-                map(str, [column for row in x for column in row]))])
-
-        print(f"ITTERATION {i}")
-        i += 1
 
 
 # General Search takes Initial State, GoalState,SearchStrategy,Heuristics
-
 
 def general_search(istate, goal_state, search_strategy, heuristic=None):
     print(f"General Search has been called with Heursitic = {heuristic}")
@@ -334,32 +324,30 @@ def main():
     # GoalState = list("12345678B")
 
     # Test Cases for BFS
-    # GoalState = list("12345678B")
     # current_state = list("123456B78")
-
-    # Test Case for BEST FIRST SEARCH
+    # GoalState = list("12345678B")
+    
+    # Test Case for BEST FIRST SEARCH and A STAR GIVEN BY THE PROF
     GoalState = list("12345678B")
-    current_state = list("25B461783")
-    #current_state = list("7421B5863")
+    current_state = list("4137B5826")
+
+    # The Use of the Numpy module allows this easy conversion from string to a 2 d array. Then , we can run some heuristics
+
     GoalStateArray = np.array(GoalState).reshape((-1, 3))
     current_state = np.array(current_state).reshape((-1, 3))
 
-    print(manhatten_distance(current_state, GoalStateArray))
-    print(my_heuristic(current_state, GoalStateArray))
+    # print(f"The Manhatten Distance for the initial state is {manhatten_distance(current_state, GoalStateArray)}")
+    # print(f"The Inversion for the initial state is {permutation_inversion(current_state, GoalStateArray)}")
+    # print(
+    #      f"The Hamming Distance is :  {hamming_distance(current_state,GoalStateArray)}")
+    # print(f"My heuristics gives a value of {my_heuristic(current_state,GoalStateArray)} for the inital state")
+   
 
-# The Use of the Numpy module allows this easy conversion. Then , we can run some heuristics
-
-    print(
-        f"The Hamming Distance is :  {hamming_distance(current_state,GoalStateArray)}")
-
-    # print("This is the Initial State")
-    # print(current_state)
-    # # time.sleep(3)
-
-    # general_search(current_state,GoalStateArray,BreathFirstSearch)
-
+    #This function would be called for either DFS or BFS 
+    #general_search(current_state,GoalStateArray,BreathFirstSearch)
+    
     general_search(current_state, GoalStateArray,
-                   BestFirst, manhatten_distance)
+                   Astar, manhatten_distance)
 
 
 if __name__ == '__main__':
